23e31d31ea41b3cfe56c98df2ba7a080076197de
Shajib <sajidislam729@gmail.com>
2021-08-22 23:47:45 +0600

    file changed

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.use('/api', require('./routes/user'))
 app.use('/api', require('./routes/conversation'))
 app.use('/api', require('./routes/message'))
 
-// 3: setup in heroku 
-// if (process.env.NODE_ENV !== 'production') {
-//     app.use(express.static(path.join(__dirname,"/my-app/build/")))
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname,"my-app",'build','index.html'))
-//     })
-// }
+3: setup in heroku 
+if (process.env.NODE_ENV !== 'production') {
+    app.use(express.static(path.join(__dirname,"/my-app/build/")))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname,"my-app",'build','index.html'))
+    })
+}
 
 http.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
