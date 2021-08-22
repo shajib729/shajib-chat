49d8247d3b95e31ea1a572e0d000964fa03dba80
Shajib <sajidislam729@gmail.com>
2021-08-23 00:25:32 +0600

    commit message

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const port = process.env.PORT || 5000
 const path = require('path')
 const cookieParser = require('cookie-parser')
 // const io=require("sockekt.io")
+const cors = require("cors");
 
 require('./db/conn')
 app.use(cookieParser())
+app.use(cors());
 //parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,14 +20,20 @@ app.use('/api', require('./routes/conversation'))
 app.use('/api', require('./routes/message'))
 
 // 3: setup in heroku 
-if (process.env.NODE_ENV === "production") {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, "my-app/build")));
-    // Handle React routing, return all requests to React app
-    app.get("*", function (req, res) {
-      res.sendFile(path.join(__dirname, "my-app/build", "index.html"));
-    });
-  }
+// if (process.env.NODE_ENV === "production") {
+//     // Serve any static files
+//     app.use(express.static(path.join(__dirname, "my-app/build")));
+//     // Handle React routing, return all requests to React app
+//     app.get("*", function (req, res) {
+//       res.sendFile(path.join(__dirname, "my-app/build", "index.html"));
+//     });
+// }
+
+app.use(express.static(path.resolve(__dirname, "./my-app/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./my-app/build", "index.html"));
+});
 
 http.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
