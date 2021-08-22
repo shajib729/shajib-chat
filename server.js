26ce708c5d154cf3656e220c00c2fa918e67330f
Shajib <sajidislam729@gmail.com>
2021-08-22 11:14:51 +0600

    a

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use('/', require('./routes/user'))
 
 // 3: setup in heroku 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV == 'production') {
     app.use(express.static(path.join(__dirname,"/my-app/build/")))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname,"my-app",'build','index.html'))
